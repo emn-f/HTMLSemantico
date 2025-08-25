e47a5a8d641dcedc928d0c50eef09facdb60b89a
emanuel <95362058+emn-f@users.noreply.github.com>
2025-08-25 20:57:05 -0300

    feat: icon adicionado

--- aula3/script/script.js
+++ aula3/script/script.js
@@ -1,5 +1,4 @@
 window.onload = function () {
-    alert('hey :)');
     var itens = document.querySelectorAll('.navbar div')
     itens.forEach(function(item) {
         item.onclick = function () {
