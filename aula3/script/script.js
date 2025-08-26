bd61c32d8cd1ce44ca344ca84e06d2e9a5b7376d
emanuel <95362058+emn-f@users.noreply.github.com>
2025-08-25 21:03:01 -0300

    feat: texto volta ao conteudo original após 2s

--- aula3/script/script.js
+++ aula3/script/script.js
@@ -1,8 +1,11 @@
 window.onload = function () {
     var itens = document.querySelectorAll('.navbar div')
-    itens.forEach(function(item) {
+    itens.forEach(function (item) {
         item.onclick = function () {
-            this.textContent = 'clicado';
+            var textoOriginal = this.textContent;
+            this.textContent = 'CLICOU!';
+            var el = this;
+            setTimeout(function () { el.textContent = textoOriginal }, 500);
         }
     })
 };  
